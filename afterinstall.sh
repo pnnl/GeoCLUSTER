bc528d0d136cc7f733675df7ae608a5028fc0472
mmacduff <matt.macduff@pnnl.gov>
2024-12-17 16:32:08 -0800

    fix

--- afterinstall.sh
+++ afterinstall.sh
@@ -2,4 +2,6 @@
 /bin/rm -rf /www/GeoCLUSTER.orig
 /bin/mv /www/GeoCLUSTER /www/GeoCLUSTER.orig
 /bin/mv /www/GeoCLUSTER.new /www/GeoCLUSTER
+chown -Rh apache /www/GeoCLUSTER/dash_app
+chmod o+rx /www/GeoCLUSTER
 systemctl restart httpd
