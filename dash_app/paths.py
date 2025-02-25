ffc6cb78f69278769e228db90a4bd1d09e38d341
buch688 <alexander.buchko@pnnl.gov>
2025-02-25 10:23:12 -0800

    improving requirements

--- dash_app/paths.py
+++ dash_app/paths.py
@@ -1,5 +1,5 @@
 import os
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
@@ -7,7 +7,7 @@ localpath = ""
 internal_pnnl_path = "/var/www/html/dash_app/"
 aws_path = "/www/GeoCLUSTER/dash_app/"
 
-# load_dotenv()
+load_dotenv()
 print(os.environ)
 deployment_type = os.getenv("deployment_type", "aws")
 absolute_path = localpath if deployment_type == "local" else aws_path
