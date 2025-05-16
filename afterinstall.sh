a9a1ec08c7929055234f01021dc602c6598d588a
buch688 <alexander.buchko@pnnl.gov>
2025-05-16 12:06:51 -0700

    test-trying-to-fix, adding intallation for python3 as well

--- afterinstall.sh
+++ afterinstall.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 /bin/rm -rf /www/GeoCLUSTER.orig
 python3.11 -m pip install -r /www/GeoCLUSTER.new/dash_app/requirements.txt
+python3 -m pip install -r /www/GeoCLUSTER.new/dash_app/requirements.txt
 cd /www/GeoCLUSTER.new/dash_app/data; python3.11 download_hdf5.py
 /bin/mv /www/GeoCLUSTER /www/GeoCLUSTER.orig
 /bin/mv /www/GeoCLUSTER.new /www/GeoCLUSTER
