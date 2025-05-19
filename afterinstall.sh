a328c83c218df790c636404bde7ceb5398250538
buch688 <alexander.buchko@pnnl.gov>
2025-05-19 12:54:40 -0700

    updating after install to use venv explicitly

--- afterinstall.sh
+++ afterinstall.sh
@@ -2,16 +2,15 @@
 /bin/rm -rf /www/GeoCLUSTER.orig
 
 # activating the virtual environment
-source /www/geoVenv/bin/activate
+/www/geoVenv/bin/pip install --upgrade pip
+/www/geoVenv/bin/pip install -r /www/GeoCLUSTER.new/dash_app/requirements.txt
 
-#installing the requirements
-pip install /www/GeoCLUSTER.new/dash_app/requirements.txt
+# downloading hdf5 if it is not already downloaded
+cd /www/GeoCLUSTER.new/dash_app/data; /www/geoVenv/bin/python download_hdf5.py
 
 #giving apache permission to the venv
 chown -R apache:apache /www/geoVenv
 
-# downloading hdf5 if it is not already downloaded
-cd /www/GeoCLUSTER.new/dash_app/data; python3 download_hdf5.py
 
 /bin/mv /www/GeoCLUSTER /www/GeoCLUSTER.orig
 /bin/mv /www/GeoCLUSTER.new /www/GeoCLUSTER
