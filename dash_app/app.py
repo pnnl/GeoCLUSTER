7816427a599905f25ca2a8bdb761f6cac067ee4e
Bernat, Anastasia <anastasia.bernat@pnnl.gov>
2025-06-26 09:39:22 -0700

    Debug false.

--- dash_app/app.py
+++ dash_app/app.py
@@ -1761,7 +1761,7 @@ if __name__ == '__main__':
     app.run_server(
         # host="127.0.0.1",
         port=8060,
-        debug=True, # needs to be False in production
+        debug=False, # needs to be False in production
         ssl_context="adhoc"
     )
 
